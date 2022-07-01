59d053caa1696a5d60c1760b489254fade0f0549
smartdev0218 <antonio.krijan0218@gmail.com>
2022-07-01 15:52:12 +0200

    fixed contract

--- contracts/NFTAuction.sol
+++ contracts/NFTAuction.sol
@@ -302,13 +302,7 @@ contract NFTAuction {
         _;
     }
 
-    modifier isNotASale(address _nftContractAddress, uint256 _tokenId) {
-        require(
-            !_isASale(_nftContractAddress, _tokenId),
-            "Not applicable for a sale"
-        );
-        _;
-    }
+    
 
     /**********************************/
     /*╔═════════════════════════════╗
