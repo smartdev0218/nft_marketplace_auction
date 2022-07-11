46b4717d43b5823b7361974f4b050fb3acb79769
smartdev0218 <antonio.krijan0218@gmail.com>
2022-07-11 21:38:02 +0200

    fixed

--- contracts/NFTAuction.sol
+++ contracts/NFTAuction.sol
@@ -312,6 +312,14 @@ contract NFTAuction {
 
     
     // constructor
+    constructor() {
+        defaultBidIncreasePercentage = 100;
+        defaultAuctionBidPeriod = 86400; //1 day
+        minimumSettableIncreasePercentage = 100;
+        maximumMinPricePercentage = 8000;
+    }
+
+    /*╔══════════════════════════════╗
       ║    AUCTION CHECK FUNCTIONS   ║
       ╚══════════════════════════════╝*/
     function _isAuctionOngoing(address _nftContractAddress, uint256 _tokenId)
